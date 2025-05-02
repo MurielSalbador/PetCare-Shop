a9ce09aa859fde9870e52791ab33baeefe6d77ed
MurielSalbador <salbadormuriel@gmail.com>
2025-05-02 18:06:30 -0300

    Second  commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import FormValidation from './component/formValidation/FormValidation'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <FormValidation />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/formValidation/FormValidation.jsx
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './FormValidation.css';
+
+function FormValidation() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+    <>
+      <div className="login-form">
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <h3 className="text-center mb-4">Iniciar sesión</h3>
+
+          <Form.Group className="mb-3" controlId="formBasicSelect">
+            <Form.Label>Seleccione una opción</Form.Label>
+            <Form.Select className="select-button">
+              <option value="1">Usuario</option>
+              <option value="2">Gestión</option>
+              <option value="3">Administrador</option>
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Correo electrónico</Form.Label>
+            <Form.Control
+              required
+              type="email"
+              placeholder="Tu correo"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Control.Feedback type="invalid">
+              Ingresá un correo válido.
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control
+              required
+              type="password"
+              placeholder="Tu contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              minLength={6}
+            />
+            <Form.Control.Feedback type="invalid">
+              La contraseña debe tener al menos 6 caracteres.
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <div className="button-group">
+            <Button type="submit" className="custom-button-login">
+              Ingresar
+            </Button>
+
+            <Button type="submit" className="custom-button-register">
+              Registrarse
+            </Button>
+          </div>
+        </Form>
+      </div>
+
+      <div className="faq-recomendation">
+        <p className="text-center">
+          Si olvidaste tu contraseña, hacé click en{" "}
+          <a href="/recuperar" className="custom-button-faq">
+            Recuperarla
+          </a>{" "}
+          y seguí las instrucciones para recuperarla.
+        </p>
+
+        <p className="text-center">
+          Para más preguntas, hacé click en{" "}
+          <a href="/faq" className="custom-button-faq">
+            Preguntas Frecuentes
+          </a>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default FormValidation;
